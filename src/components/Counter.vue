12903d200c1788176d0a04d1fc3bd119f8c22489
Auri-Daine <auristark0gmail.com>
2021-03-30 23:30:17 +0200

    The Second Commit with The new Component

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>This is a Counter</h1>
+    <p>Click to Increment.</p>
+    <p>You have Clicked {{ total }} Times.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      total: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.total++;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
